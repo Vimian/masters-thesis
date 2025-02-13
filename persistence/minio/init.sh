5f3496775a24ea18aaf6e9dafccfbda57212aee3
Casper Fenger Jensen <43348526+Vimian@users.noreply.github.com>
2025-02-13 15:05:10 +0100

    save

--- persistence/minio/init.sh
+++ persistence/minio/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Set Minio environment variables
+# set minio environment variables
 MINIO_ROOT_USER=${MINIO_ROOT_USER}
 MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD}
 MINIO_SERVER=${MINIO_SERVER:-minio:9000}
 
-# Wait for Minio to be ready
+# wait for minio to be ready
 while true; do
   mc ls minio > /dev/null 2>&1
   if [[ $? -eq 0 ]]; then
@@ -19,12 +19,12 @@ done
 
 sleep 5
 
-# Set Minio alias
+# set minio alias
 mc alias set minio http://$MINIO_SERVER $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
-# Create bucket
+# create bucket
 BUCKET_NAME="benchmark"
 mc mb minio/"$BUCKET_NAME"
 
-# Upload original files
+# upload original files
 mc cp /minio/original minio/"$BUCKET_NAME" --recursive
--- pkg/helloworld/main.go
+++
@@ -1,5 +0,0 @@
-package helloworld
-
-func HelloWorld() {
-	println("Hello, World! From helloworld package")
-}
