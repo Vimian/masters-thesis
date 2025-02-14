347dac1aa3e89b545749473984979a27730efa42
Casper Fenger Jensen <43348526+Vimian@users.noreply.github.com>
2025-02-14 10:20:10 +0100

    save

--- persistence/minio/init.sh
+++ persistence/minio/init.sh
@@ -24,7 +24,7 @@ mc alias set minio http://$MINIO_SERVER $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 # create bucket
 BUCKET_NAME="benchmark"
-mc mb minio/"$BUCKET_NAME"
+mc mb minio/"$BUCKET_NAME" #
 
 # upload original files
 mc cp /minio/original minio/"$BUCKET_NAME" --recursive
