65a726a0ec26afd2e62ce86bfd7b4dd0f5f5e74c
Casper Fenger Jensen <43348526+Vimian@users.noreply.github.com>
2025-02-14 10:31:43 +0100

    save

--- persistence/minio/init.sh
+++ persistence/minio/init.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-
 # set minio environment variables
-MINIO_ROOT_USER=${MINIO_ROOT_USER}
-MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD}
-MINIO_SERVER=${MINIO_SERVER:-minio:9000}
-
+MINIO_ROOT_USER=${MINIO_ROOT_USER} #
+MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD} #
+MINIO_SERVER=${MINIO_SERVER:-minio:9000} #
 # wait for minio to be ready
-while true; do
-  mc ls minio > /dev/null 2>&1
-  if [[ $? -eq 0 ]]; then
-    echo "mc is ready."
-    break
-  else
-    echo "mc is not ready yet!"
-    sleep 5
-  fi
-done
-
+while true; do #
+  mc ls minio > /dev/null 2>&1 #
+  if [[ $? -eq 0 ]]; then #
+    echo "mc is ready." #
+    break #
+  else #
+    echo "mc is not ready yet!" #
+    sleep 5 #
+  fi #
+done #
+# sleep for 5 seconds
 sleep 5
-
 # set minio alias
-mc alias set minio http://$MINIO_SERVER $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-
+mc alias set minio http://$MINIO_SERVER $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD #
+#
 # create bucket
-BUCKET_NAME="benchmark"
+BUCKET_NAME="benchmark" #
 mc mb minio/"$BUCKET_NAME" #
-
+#
 # upload original files
-mc cp /minio/original minio/"$BUCKET_NAME" --recursive
+mc cp /minio/original minio/"$BUCKET_NAME" --recursive #
