8f7ac393958703edf575718a9f76c70b50b91b7e
tobber00 <39513708+tobber00@users.noreply.github.com>
2025-04-24 13:31:33 +0200

    version 2

--- cmd/cloud-benchmark/main.go
+++ cmd/cloud-benchmark/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
@@ -13,6 +12,7 @@ import (
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+
 	"github.com/vimian/masters-thesis/cmd/cloud-benchmark/persistence"
 )
 
@@ -44,11 +44,16 @@ func main() {
 		log.Fatalf("error parsing runs: %v", err)
 	}
 
-	//test(numberOfRuns, "premium", "DefaultEndpointsProtocol=https;AccountName=masterthesispremiumtier;AccountKey=iqQX4EujDpxb2+L64cAsEhy96aduGbgWh/AdcweoZFsnwVZS8P0Wmr/QPBiqadaq9rsn2xac+05P+AStV0atxg==;EndpointSuffix=core.windows.net", filesnames)
-	test(numberOfRuns, "hot", "DefaultEndpointsProtocol=https;AccountName=masterthesishottier;AccountKey=GlmPvlSzv3nEUoo36RYWNsfOCfaPWrjJlg/B2oqgvzeTq/kH1RSC+oTQI9M4SnyxPGx44Bm6vJmj+AStdsXgVA==;EndpointSuffix=core.windows.net", filesnames)
-	//test(numberOfRuns, "cool", "DefaultEndpointsProtocol=https;AccountName=masterthesiscooltier;AccountKey=+RF+VOPKw0Ju7L3n/POuA4zbLWYwMYud9CWQEgcQ9ZheBQbN2Nnx0CDRmDZmpTVx8OlBrXlmuAe8+AStYw9tqQ==;EndpointSuffix=core.windows.net", filesnames)
-	//test(numberOfRuns, "cold", "DefaultEndpointsProtocol=https;AccountName=masterthesiscoldtier;AccountKey=1lyP4P4LQzbsV/PB0jsFyO77Gm/CVnQvDAfOF87en4J2U8+dGSyG0aAfebvMAqX/wKXIsxRckX/k+AStn8nDNA==;EndpointSuffix=core.windows.net", filesnames)
+	//premium do not work with the current solution, don't know if it can?
+	//maPremiumKey := os.Getenv("MA_PREMIUM_TIER")
+	maHotKey := os.Getenv("MA_HOT_TIER")
+	//maCoolKey := os.Getenv("MA_COOL_TIER")
+	//maColdKey := os.Getenv("MA_COLD_TIER")
 
+	//test(numberOfRuns, "premium", maPremiumKey, filesnames)
+	test(numberOfRuns, "hot", maHotKey, filesnames)
+	//test(numberOfRuns, "cool", maCoolKey, filesnames)
+	//test(numberOfRuns, "cold", maColdKey, filesnames)
 }
 
 func getFileNames(directory string) []string {
@@ -76,19 +81,14 @@ func test(numberOfRuns int, tierName string, connectionString string, fileNames 
 func run(run int, tierName string, connectionString string, fileName string) {
 	containerName := "uploadcontainer"
 	localFilePath := "./files/" + fileName
+	downloadedFilePath := "./downloads/" + fileName
 
 	// Create a BlobServiceClient
-	serviceClient, err := azblob.NewClientFromConnectionString(connectionString, nil)
+	client, err := azblob.NewClientFromConnectionString(connectionString, nil)
 	if err != nil {
 		fmt.Println("Error creating service client:", err)
 		return
 	}
-
-	// Get BlobContainerClient
-	containerClient := serviceClient.ServiceClient().NewContainerClient(containerName)
-
-	// Get BlockBlobClient
-	blobClient := containerClient.NewBlockBlobClient(fileName)
 
 	// Upload the file
 	fmt.Println("Starting file upload...")
@@ -106,15 +106,19 @@ func run(run int, tierName string, connectionString string, fileName string) {
 	}
 
 	fileSize := fileInfo.Size()
+
+	// Hash the original file
 	originalHash := sha256.New()
 	if _, err := io.Copy(originalHash, file); err != nil {
 		fmt.Println("Error hashing file:", err)
 		return
 	}
 	fmt.Println("original hash:", hex.EncodeToString(originalHash.Sum(nil)))
+
 	uploadStartTime := time.Now().UnixNano()
 
-	_, err = blobClient.UploadStream(context.Background(), file, nil)
+	//upload the file to the blob storage
+	_, err = client.UploadFile(context.Background(), containerName, fileName, file, nil)
 	if err != nil {
 		fmt.Println("Error uploading file:", err)
 		return
@@ -127,67 +131,35 @@ func run(run int, tierName string, connectionString string, fileName string) {
 	// Download the file
 	fmt.Println("\nStarting file download...")
 
-	downloadedFilePath := "./downloads/" + fileName
+	downloadStartTime := time.Now().UnixNano()
+	fmt.Println(downloadStartTime)
+
 	downloadFile, err := os.Create(downloadedFilePath)
 	if err != nil {
-		fmt.Println("Error creating download file:", err)
+		fmt.Println("Error creating local file:", err)
 		return
 	}
 	defer downloadFile.Close()
 
-	downloadStartTime := time.Now().UnixNano()
-	/*
-		get, err := blobClient.DownloadStream(context.Background(), nil)
-		if err != nil {
-			fmt.Println("Error downloading file:", err)
-			return
-		}
-		defer get.Body.Close()
-
-		_, err = io.Copy(downloadFile, get.Body)
-		if err != nil {
-			fmt.Println("Error copying downloaded data to file:", err)
-			return
-		}
-	*/
-
-	// Download the blob
-	//TODO
-	//LOOK HERE
-	get, err := blobClient.DownloadStream(context.Background(), nil)
+	_, err = client.DownloadFile(context.Background(), containerName, fileName, downloadFile, nil)
 	if err != nil {
 		fmt.Println("Error downloading file:", err)
 		return
 	}
 
-	downloadedData := bytes.Buffer{}
-	retryReader := get.NewRetryReader(context.Background(), &azblob.RetryReaderOptions{})
-	_, err = downloadedData.ReadFrom(retryReader)
-	if err != nil {
-		fmt.Println("Error reading downloaded data:", err)
-		return
-	}
-
-	err = retryReader.Close()
-	if err != nil {
-		fmt.Println("Error closing retry reader:", err)
-		return
-	}
+	downloadEndTime := time.Now().UnixNano()
+	downloadDuration := downloadEndTime - downloadStartTime
 
 	//Hash the downloaded file
 	downloadedHash := sha256.New()
-	if _, err := io.Copy(downloadedHash, &downloadedData); err != nil {
+	if _, err := io.Copy(downloadedHash, downloadFile); err != nil {
 		fmt.Println("Error hashing file:", err)
 		return
 	}
 
-	downloadEndTime := time.Now().UnixNano()
-	downloadDuration := downloadEndTime - downloadStartTime
-	fmt.Printf("File downloaded successfully to '%s'.\n", downloadedFilePath)
-
 	// Delete the blob
 	fmt.Println("\nDeleting the blob...")
-	_, err = blobClient.Delete(context.Background(), nil)
+	_, err = client.DeleteBlob(context.Background(), containerName, fileName, nil)
 	if err != nil {
 		fmt.Println("Error deleting blob:", err)
 		return
@@ -195,15 +167,13 @@ func run(run int, tierName string, connectionString string, fileName string) {
 	fmt.Printf("Blob '%s' deleted successfully from container '%s'.\n", fileName, containerName)
 
 	// Delete the downloaded file
-	/*
-		downloadFile.Close()
-		err = os.Remove(downloadedFilePath)
-		if err != nil {
-			fmt.Println("Error deleting downloaded file:", err)
-			return
-		}
-		fmt.Printf("Downloaded file '%s' deleted successfully.\n", downloadedFilePath)
-	*/
+	downloadFile.Close()
+	err = os.Remove(downloadedFilePath)
+	if err != nil {
+		fmt.Println("Error deleting downloaded file:", err)
+		return
+	}
+	fmt.Printf("Downloaded file '%s' deleted successfully.\n", downloadedFilePath)
 
 	// Insert results into PostgreSQL
 	result := persistence.CloudResult{
@@ -224,4 +194,5 @@ func run(run int, tierName string, connectionString string, fileName string) {
 		log.Fatalf("error inserting measurement: %v", err)
 	}
 	fmt.Printf("Inserted measurement for run %d and tier %s into PostgreSQL.\n", run, tierName)
+
 }
